72d6fd0d42242f5cf18a88242bee6c3db73083be
Robert Greener <robert.greener@kcl.ac.uk>
2017-03-18 17:55:20 +0000

    Moved SightingsAtStates
    SightingsAtStates has been moved to an inner class of PredictedNextStateStatistic

--- ppa-major-coursework/src/controller/statistics/PredictedNextStateStatistic.java
+++ ppa-major-coursework/src/controller/statistics/PredictedNextStateStatistic.java
@@ -1,9 +1,10 @@
 package controller.statistics;
 
 import api.ripley.Incident;
-import models.SightingsAtStates;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * PredictedNextStateStatistic is a Statistic that gets the state where the next sighting is predicted to take place
@@ -58,4 +59,62 @@ public class PredictedNextStateStatistic extends Statistic{
         // Return the state with the highest number of sightings
         return sightingsAtStates.getHighestSightingState();
     }
+
+    /**
+     * SightingsAtState is an inner class used for storing the number of sightings at each state,
+     * and getting the state with the highest number of sightings
+     *
+     * @author Robert Greener
+     */
+    private class SightingsAtStates {
+        /**
+         * numberOfSightings is a HashMap where the key is the name of the state,
+         * and the value is the number of sightings at that state
+         */
+        private HashMap<String, Integer> numberOfSightings;
+
+        /**
+         * Creates a new SightingsAtStates
+         */
+        public SightingsAtStates() {
+            // Initialise the numberOfSightings HashMap
+            numberOfSightings = new HashMap<>();
+        }
+
+        /**
+         * Adds a sighting at a state
+         * @param stateName the name of the state
+         */
+        public void addSighting(String stateName) {
+            // If the state already exists in numberOfSightings
+            if (numberOfSightings.containsKey(stateName)) {
+                // Increase the value by 1
+                numberOfSightings.put(stateName, numberOfSightings.get(stateName) + 1);
+            }
+            // Otherwise
+            else {
+                // Set the number of sightings for that state to 1
+                numberOfSightings.put(stateName, 1);
+            }
+        }
+
+        /**
+         * Gets the state with the most UFO sightings
+         * @return the state with the highest number of UFO sightings
+         */
+        public String getHighestSightingState() {
+            // Set the initial highestState key-value pair to a random pair from numberOfSightings
+            Map.Entry<String, Integer> highestState = numberOfSightings.entrySet().iterator().next();
+
+            // For each key-value pair in the numberOfSightings' entrySet
+            for (Map.Entry<String, Integer> entry : numberOfSightings.entrySet()) {
+                // If there are more sightings at this entry, set this entry as the highest state
+                if (entry.getValue() > highestState.getValue()) highestState = entry;
+            }
+
+            // Return the key of highestState (which is the state name)
+            return highestState.getKey();
+        }
+    }
+
 }
