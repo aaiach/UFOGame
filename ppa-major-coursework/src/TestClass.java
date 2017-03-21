d6173ec4cd88602273c386186203dd88f2a6434e
Robert Greener <robert.greener@kcl.ac.uk>
2017-03-21 11:59:45 +0000

    TestClass updated

--- ppa-major-coursework/src/TestClass.java
+++ ppa-major-coursework/src/TestClass.java
@@ -1,7 +1,12 @@
+import api.ripley.Incident;
+import controller.statistics.USStates;
 import controller.statistics.YouTubeAPI;
+import models.SightingsAtStates;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by robert on 07/03/17.
@@ -18,5 +23,14 @@ public class TestClass {
         JSONObject jsonObject = new JSONObject(resp);
         int totalResults = jsonObject.getJSONObject("pageInfo").getInt("totalResults");
         System.out.println(totalResults);
+
+
+
+        // new test
+        List<Incident> incidentList = new ArrayList<Incident>();
+
+        SightingsAtStates sightingsAtStates = new SightingsAtStates(incidentList);
+        HashMap<String, Integer> numberOfSightings = sightingsAtStates.getNumberOfSightings();
+        int numberOfSightingsInCalifornia = numberOfSightings.get(USStates.getFullStateName("CA"));
     }
 }
