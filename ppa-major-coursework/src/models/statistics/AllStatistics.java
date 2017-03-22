f88a3e530a9582a4d681d22ec03c2b0ab136def4
Robert Greener <robert.greener@kcl.ac.uk>
2017-03-22 11:23:10 +0000

    Revert "Merged origin/statistic-panel"
    This reverts commit d85feebda105a7fa041a12ad18e19db8b968150c.

--- ppa-major-coursework/src/models/statistics/AllStatistics.java
+++ ppa-major-coursework/src/models/statistics/AllStatistics.java
@@ -18,35 +18,31 @@ public class AllStatistics {
         this.statistics.addAll(statistics);
     }
 
-    public void set (int index, Statistic statistic) {
+    public void set(int index, Statistic statistic) {
         this.statistics.set(index, statistic);
     }
 
-    public void add (Statistic s) {
+    public void add(Statistic s) {
         this.statistics.add(s);
     }
 
-    public Statistic get (int index) {
+    public Statistic get(int index) {
         return this.statistics.get(index);
     }
 
-    public Statistic remove (int index) {
+    public Statistic remove(int index) {
         return this.statistics.remove(index);
     }
 
-    public boolean remove (Statistic s) {
+    public boolean remove(Statistic s) {
         return this.statistics.remove(s);
     }
 
-    public int indexOf (Statistic s) {
+    public int indexOf(Statistic s) {
         return this.statistics.indexOf(s);
     }
 
-    public int size () {
+    public int size() {
         return this.statistics.size();
     }
 }
-
-    public AllStatistics(List<Statistic> statistics) {
-        this.statistics.addAll(statistics);
-        this.statistics = new ArrayList<>();
