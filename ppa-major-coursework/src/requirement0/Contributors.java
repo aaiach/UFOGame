758c2e15e7bed39f3f2f557a5e131b19e3f31846
Robert Greener <robert.greener@kcl.ac.uk>
2017-03-22 21:36:23 +0000

    Added comments to Contributors.java

--- ppa-major-coursework/src/requirement0/Contributors.java
+++ ppa-major-coursework/src/requirement0/Contributors.java
@@ -1,13 +1,22 @@
 package requirement0;
 
+/**
+ * This class prints the names of the 4 contributors
+ *
+ * @author Robert Greener
+ * @author Stevan Warren
+ * @author Shantanu Jha
+ * @author Adriel Aiach
+ */
 public class Contributors {
+    /**
+     * Prints the names of the 4 contributors
+     * @param args command line arguments are ignored
+     */
 	public static void main(String[] args) {
-		
 		System.out.println("Robert Greener");
 		System.out.println("Stevan Warren");
 		System.out.println("Shantanu Jha");
 		System.out.println("Adriel Aiach");
-
-		
 	}
 }
