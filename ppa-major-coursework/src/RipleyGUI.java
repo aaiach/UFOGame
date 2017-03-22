101355ff59597879ba9c55aaf22b43902d94d792
Robert Greener <robert.greener@kcl.ac.uk>
2017-03-22 23:41:47 +0000

    Added class with main method

---
+++ ppa-major-coursework/src/RipleyGUI.java
@@ -0,0 +1,18 @@
+import controller.MainController;
+
+/**
+ * This class contains the entry point for the program
+ *
+ * @author Robert Greener
+ */
+public class RipleyGUI {
+
+    /**
+     * This method is the entry point for the program
+     * @param args Command line args are ignored
+     */
+    public static void main (String[] args) {
+        // By making a MainController the program will run
+        MainController controller = new MainController();
+    }
+}
