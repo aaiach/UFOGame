6618543c16d364b7806ba29ae710af32bcb11361
k1631436 <stevan.warren@kcl.ac.uk>
2017-03-23 20:46:24 +0000

    Added my statistic (proportion of sightings from the US)

--- ppa-major-coursework/src/controller/statistics/StevanWarrenStatistic.java
+++ ppa-major-coursework/src/controller/statistics/StevanWarrenStatistic.java
@@ -1,29 +1,49 @@
 package controller.statistics;
 
+import java.util.List;
+
+import api.ripley.Ripley;
+
 /**
- * TODO: Implement this statistic
+ * This statistic finds the proportion of all sightings that occurred in the US.
  *
  * @author Stevan Warren
  */
 public class StevanWarrenStatistic extends Statistic {
-
+	private List<Incident> incidents;
+	
     /**
      * Creates a new StevanWarrenStatistic
      */
-    public StevanWarrenStatistic() {
+    public StevanWarrenStatistic(List<Incident> incidents) {
         // Calls the superclass constructor, setting the name of the statistic
-        super("Stevan Warren's Statistic");
+        super("Percentage of US sightings");
+        this.incidents = incidents;
 
         // Sets the data of the statistic, to the value returned by calculateData
         setData(calculateData());
     }
 
     /**
-     * Calculates the date for this statistic
-     * @return always returns "Not Implemented"
+     * Calculates the proportion of sightings in the US.
+     * @return Percentage of sightings that occurred in the US
      */
     @Override
     protected String calculateData() {
-        return "Not implemented";
+    	// To find the proportion, find the total number of sightings where the state is not specified
+    	// divide it by the total number of sightings and muliply by 100.
+    	int nonUSSightings = 0;
+    	for (Incident incident : incidents) {
+    		if (!incident.getState().equals("Not specified.")) {
+    			nonUSSightings++;
+    		}
+    	}
+    	
+    	double proportion = nonUSSightings / incidents.size() * 100;
+    	
+    	// The end result will be rounded to 2 decimal places.
+    	proportion = Math.round(proportion * 100) / 100;
+        return proportion + "%";
+        
     }
 }
