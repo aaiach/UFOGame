712b5ac3e61437244a7a4ec4d4542da9d864c754
Shantanu S Jha <shantanu.s.jha@kcl.ac.uk>
2017-03-24 02:18:21 +0000

    Implemented the functionality for my statistic
    The source code in the rest of the file hadn't gone through in the
    previous commit.

--- ppa-major-coursework/src/controller/statistics/DrinkingHoursSightingsStatistic.java
+++ ppa-major-coursework/src/controller/statistics/DrinkingHoursSightingsStatistic.java
@@ -1,5 +1,8 @@
 package controller.statistics;
 
+import api.ripley.Incident;
+import java.util.List;
+
 /**
  * This statistic determines the percentage of UFO sightings reported during drinking hours,
  * i.e. between 5pm and 11pm, in which the people were most likely to be intoxicated.
@@ -7,27 +10,52 @@ package controller.statistics;
  * This time period was officially categorized by the Economist, who used the statistics
  * compiled by the National UFO Reporting Center (NUFORC) to analyze 'peak times' for UFO reports.
  *
- * @author Shantanu Jha
+ * @author Shantanu Shekhar Jha
  */
-public class ShantanuJhaStatistic extends Statistic {
+public class DrinkingHoursSightingsStatistic extends Statistic {
 
+    private List<Incident> incidents;
+    
     /**
-     * Creates a new ShantanuJhaStatistic
+     * Creates a new DrinkingHoursSightingsStatistic
      */
-    public ShantanuJhaStatistic() {
+    public DrinkingHoursSightingsStatistic() {
         // Calls the superclass constructor, setting the name of the statistic
-        super("Shantanu Jha's Statistic");
+        super("Sightings during drinking hours (5pm-11pm)");
 
         // Sets the data of the statistic, to the value returned by calculateData
         setData(calculateData());
     }
 
     /**
-     * Calculates the date for this statistic
+     * Calculates the data for this statistic
      * @return always returns "Not Implemented"
      */
     @Override
     protected String calculateData() {
-        return "Not implemented";
+        
+        int drinkingHoursSightings = 0;
+        
+        for (Incident incident : incidents) {
+            
+            String dateAndTime = incident.getDateAndTime();
+            String[] dateAndTimeArray = dateAndTime.split(" ");
+            String time = dateAndTimeArray[1];
+            
+            String hourString = time.substring(0, 2);
+            int hour = Integer.parseInt(hourString);
+            
+            if ((hour >= 17 ) && (hour <= 23))
+                drinkingHoursSightings++;
+        
+        }
+        
+        int totalSightings = incidents.size();
+        double percentageOfDrinkingHoursSightings = (double) drinkingHoursSightings / totalSightings * 100;
+        double roundedPercentage = Math.round(percentageOfDrinkingHoursSightings * 10) / 10.0;
+        
+        return roundedPercentage + "%";
+        
     }
+    
 }
