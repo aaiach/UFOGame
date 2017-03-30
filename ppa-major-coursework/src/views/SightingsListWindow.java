9cf40fa10b21aab8b0db011035aba0ec6bd856ea
Shantanu Shekhar Jha <shantanu.s.jha@kcl.ac.uk>
2017-03-30 20:45:34 +0100

    Added the class file for SightingsListWindow
    Implemented the functionality for SightingsListWindow

--- ppa-major-coursework/src/views/SightingsListWindow.java
+++ ppa-major-coursework/src/views/SightingsListWindow.java
@@ -4,18 +4,21 @@
 package views;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.*;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 import models.*;
 import controller.*;
 
 /**
- * @author bunny
+ * @author Shantanu Shekhar Jha
  *
  */
 public class SightingsListWindow extends JFrame implements Observer {
@@ -24,14 +27,15 @@ public class SightingsListWindow extends JFrame implements Observer {
 	private DefaultListModel<ParsedIncident> jListModel;
 	private List<ParsedIncident> parsedIncidents;
 	
-	public SightingsListWindow(SightingsList sightingsList, SightingsListController controller) {
+	public SightingsListWindow(SightingsListController controller, SightingsList sightingsList) {
 		
 		this.sightingsList = sightingsList;
-		setTitle(sightingsList.getStateName() + "(" + sightingsList.getAbbreviatedStateName() + ")");
+		setTitle(sightingsList.getStateName() + " (" + sightingsList.getAbbreviatedStateName() + ")");
 		
 		setLayout(new BorderLayout());
 		JPanel topPanel = new JPanel();
-		topPanel.setLayout(new FlowLayout());
+		topPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		topPanel.setLayout(new GridLayout(1, 1));
 		
 		String[] sortOptions = {"-", "Date", "City", "Shape", "Duration", "Posted"};
 		JComboBox sortOptionsList = new JComboBox(sortOptions);
@@ -39,12 +43,22 @@ public class SightingsListWindow extends JFrame implements Observer {
 		
 		topPanel.add(sortOptionsList);
 		
+		JPanel bottomPanel = new JPanel();
+		bottomPanel.setBorder(new EmptyBorder(8, 8, 8, 8));
+		bottomPanel.setLayout(new GridLayout(1, 1));
+		bottomPanel.setBackground(Color.white);
+		
 		initJListModel();
-		JList jList = new JList(jListModel);
+		JList jList = new JList(jListModel);	
+		jList.addMouseListener(controller);
+		
 		JScrollPane scrollPane = new JScrollPane(jList);
+		scrollPane.setBorder(BorderFactory.createEmptyBorder());
+		
+		bottomPanel.add(scrollPane);
 		
 		add(topPanel, BorderLayout.NORTH);
-		add(scrollPane, BorderLayout.CENTER);
+		add(bottomPanel, BorderLayout.CENTER);
 		
 		setPreferredSize(new Dimension(700, 250));
 		setLocation(300, 200);
